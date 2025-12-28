28d1bfac32cddca124c4be18a016071f2d5abcb2
pavanigarlapati <pavanigarlapati05@gmail.com>
2025-12-28 19:10:43 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+const root = ReactDOM.createeRoot(document.getElementById(root"));
+root.render(
+  <BrowserRouter>
+  <App />
+  <BrowserRouter>
+  );
